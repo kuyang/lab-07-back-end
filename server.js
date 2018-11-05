0064225bc987fd055f069f673c3cfddd17ad6578
Greg Hsieh <greg.hsieh@gmail.com>
2018-11-04 22:38:42 -0500

    yelp and movies both working

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const superagent = require('superagent');
+const yelp = require('yelp-fusion');
 
 const app = express();
 
@@ -21,6 +22,49 @@ app.get('/location',(request, response) => {
 })
 
 app.get('/weather', getWeather)
+//***** To view original API list ****
+app.get('/yelpAPI', (request, response) => {
+    const url = `https://api.yelp.com/v3/businesses/search?term=food&latitude=37.8267&longitude=-122.4233`
+    superagent.get(url).set('Authorization', `Bearer eAJhDnd1F8Jpkl9H8KWN-5AxQu8U3w3r01ax1iJPOhaTPaIWDvJnbaaOxLT1YoQ5_N-osKAdG_hpDt-AyNZHneYQlaHPAFYrSeLJCily_ncsQkHKDL1m9Ed-3NTdW3Yx`)
+        .then(res => response.send(res.body))
+        .catch(err => response.send(HandleError(err)))
+})
+
+app.get('/yelp', (request, response) => {
+    const url = `https://api.yelp.com/v3/businesses/search?term=food&latitude=37.8267&longitude=-122.4233`
+    superagent.get(url).set('Authorization', `Bearer eAJhDnd1F8Jpkl9H8KWN-5AxQu8U3w3r01ax1iJPOhaTPaIWDvJnbaaOxLT1YoQ5_N-osKAdG_hpDt-AyNZHneYQlaHPAFYrSeLJCily_ncsQkHKDL1m9Ed-3NTdW3Yx`)
+        .then(res => response.send({
+            Restaurant_1: res.body.businesses[0].name,
+            yelp_rating: res.body.businesses[0].rating,
+            Info_1: res.body.businesses[0].categories[0].title,
+            Info_2: res.body.businesses[0].categories[1].title,
+            Address: res.body.businesses[0].location.display_address,
+            Phone: res.body.businesses[0].display_phone,
+            Yelp_url: res.body.businesses[0].url,
+        }))
+        .catch(err => response.send(HandleError(err)))
+})
+// ******** To view original API list *******
+app.get('/moviesAPI',(request, response)=>{
+    const url = `https://api.themoviedb.org/3/movie/now_playing?page=1&language=en-US&api_key=${process.env.MOVIES_API_KEY}`
+    superagent.get(url)
+        .then(res => response.send(res.body))
+        .catch(err => response.send(HandleError(err)))
+})
+
+app.get('/movies',(request, response)=>{
+    const url = `https://api.themoviedb.org/3/movie/now_playing?page=1&language=en-US&api_key=${process.env.MOVIES_API_KEY}`
+    superagent.get(url)
+        .then(res => response.send({
+            Title: res.body.results[0].title,
+            Overview: res.body.results[0].overview,
+            Release_date: res.body.results[0].release_date,
+            Vote_count: res.body.results[0].vote_count,
+            Vote_average: res.body.results[0].vote_average
+        }))
+        .catch(err => response.send(HandleError(err)))
+})
+
 
 // app.get('/location',(request, response) => {
 //     const url = `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAW1TEC4aep3rrsRW9Z8EYiYNOC8d387v0&address=7600+wisconsin+ave+Bethesda+MD`
@@ -78,7 +122,7 @@ function Location(lat,lng){
 
 //****** WEATHER ******
 function getWeather(request, response){
-    const url= `https://api.darksky.net/forecast/${process.env.DARKSKY_API_CODE}/${request.query.data}`
+    const url= `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${request.query.data}`
     superagent.get(url)
         .then(res => response.send(new Weather(res.body)))
         .catch(err => response.send(HandleError(err)))
@@ -90,6 +134,17 @@ function Weather(weatherObj){
     this.temperature = weatherObj.currently.temperature,
     this.forcast = weatherObj.hourly.summary
 }
+
+//*********** Yelp ******
+// function GetYelp(request, response){
+//     const url =  `https://api.yelp.com/v3/businesses/search`
+
+
+// }
+ 
+//*********** Movie DB ********
+
+
 
 //******* Error handeler ********
 
